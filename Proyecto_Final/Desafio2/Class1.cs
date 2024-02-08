6da7cfdd7fd9d9c2d14027cfb4a7192bdeb76151
2528ms <sosamariano5@gmail.com>
2024-02-08 19:46:12 -0300

    limpieza de solution para arrancar pre entregar de proyecto final

--- Proyecto_Final/Desafio2/Class1.cs
+++ Proyecto_Final/Desafio2/Class1.cs
@@ -1,7 +1,13 @@
-﻿namespace Desafio2
+﻿using DataAccess.Services;
+
+namespace Desafio2
 {
     public class Class1
     {
+        public Class1() {
+            UsuarioData.ListarUsuarios();
+        }
+       
 
     }
 }
