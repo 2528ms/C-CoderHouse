06cf953efb42ba2465d11d05eab4540a430d9cee
2528ms <sosamariano5@gmail.com>
2024-02-18 15:44:03 -0300

    commit to clear fit changes

--- Desafios_Comision50285/Clase10_CRUD_ADOnet/obj/Debug/net6.0/Clase10_CRUD_ADOnet.AssemblyInfo.cs
+++ Desafios_Comision50285/Clase10_CRUD_ADOnet/obj/Debug/net6.0/Clase10_CRUD_ADOnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Clase10_CRUD_ADOnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10de8e86418dd1749d67d5decc4fa057413e514e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Clase10_CRUD_ADOnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Clase10_CRUD_ADOnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
